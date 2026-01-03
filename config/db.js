f26430ef349e088ff7101e960f24439c04ac413c
sameera-ms <sameera@silverlineit.co>
2026-01-03 13:52:17 +0530

    Add .env.example file and update database connection URI
    - Created a new .env.example file with default PORT and MONGODB_URI.
    - Updated app.js to use PORT 5001.
    - Modified db.js to connect to the MongoDB Atlas URI instead of the local database.

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bustracking', {
+    const conn = await mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://chamikadamith9:Chamika%40200273@cluster0.bcz4z.mongodb.net/busassesment?retryWrites=true&w=majority&appName=Cluster0", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
